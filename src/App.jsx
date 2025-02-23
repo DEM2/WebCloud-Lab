10f5bfebe537fe5b3c5b61b13daab97e6d83cc5d
Juan José Campanella De Oro <juancampanelladeoro@gmail.com>
2025-02-23 17:13:29 -0500

    Footer Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/NavBar'
 import AboutUs from './Pages/About_Us'
 import Services from './Pages/Services'
 import Contact from './components/Contact';
+import Footer from './Pages/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <AboutUs />
       <Services />
       <Contact />
+      <Footer />
     </>
   )
 }
---
+++ src/Pages/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../CSS/Footer.css";
+import { FaFacebookF, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        <p className="footer-text">© 2024 WebCloud Labs. All rights reserved.</p>
+
+        <ul className="social-links">
+          <li><a href="#"><FaFacebookF /></a></li>
+          <li><a href="#"><FaInstagram /></a></li>
+          <li><a href="#"><FaLinkedinIn /></a></li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

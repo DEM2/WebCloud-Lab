c46e1c6a3f1923537f10f99f632430a515b2db7c
Juan José Campanella De Oro <juancampanelladeoro@gmail.com>
2025-02-23 03:29:38 -0500

    Bug fixes

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -13,12 +13,10 @@ function Homepage() {
 
       <header>
         <h1 className="principal-title typing">
-          <span className="color1"> Transform</span>
-          <span className="color2"> your</span>
-          <span className="color3"> Vision</span>
+          <span className="color"> Transform your vision</span>
         </h1>
         <p className="principal-text">Into a Stunning Website with Us</p>
-        <p className="principal-text">¡Let&apos;s build together your digital success story!</p>
+        <p className="principal-texts">¡Let&apos;s build together your digital success story!</p>
         <button className="contact">Contact Us</button>
       </header>
     </div>

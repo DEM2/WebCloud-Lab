b255cd5124b89e9bdef0c99ee817df61f1777c53
Juan José Campanella De Oro <juancampanelladeoro@gmail.com>
2025-02-23 02:07:10 -0500

    Contact Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Homepage from './Pages/HomePage'
 import Navbar from './components/NavBar'
 import AboutUs from './Pages/About_Us'
 import Services from './Pages/Services'
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Navbar />
       <Homepage />
       <AboutUs />
-      <Services/>
+      <Services />
+      <Contact />
     </>
   )
 }
